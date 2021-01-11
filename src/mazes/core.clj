caa8078d8760529ca647924c2582eeefa20596cb
Tom Johnson <tom@tmjohnson.co.uk>
2021-01-11 16:59:49 +0000

    Update gitignore

--- src/mazes/core.clj
+++ src/mazes/core.clj
@@ -9,17 +9,17 @@
    [mazes.algorithms :as algo]
    [mazes.utils :as utils]))
 
-(def maze (algo/recursive-backtracker (polar/new-grid 20)))
+(def maze (algo/recursive-backtracker (polar/new-grid 100)))
 (def distances (dist/dijkstra maze [5 18]))
 (pr/polar-out maze "images/polar-maze-distances.png" {:distances distances})
 (pr/polar-out maze "images/polar-maze.png")
 
-(def square (algo/hunt-and-kill (grid/new-grid 10 10)))
-(def square-distances (dist/dijkstra square [5 5]))
-(pr/png-out square "images/square-maze-distances.png" {:distances square-distances})
-(pr/png-out square "images/square-maze.png")
+;; (def square (algo/hunt-and-kill (grid/new-grid 10 10)))
+;; (def square-distances (dist/dijkstra square [5 5]))
+;; (pr/png-out square "images/square-maze-distances.png" {:distances square-distances})
+;; (pr/png-out square "images/square-maze.png")
 
-(def masked (algo/hunt-and-kill (masked/new-grid "mask.txt")))
-(def masked-distances (dist/dijkstra masked [3 3]))
-(pr/png-out masked "images/masked-maze-distances.png" {:distances masked-distances})
-(pr/png-out masked "images/masked-maze.png")
+;; (def masked (algo/hunt-and-kill (masked/new-grid "mask.txt")))
+;; (def masked-distances (dist/dijkstra masked [3 3]))
+;; (pr/png-out masked "images/masked-maze-distances.png" {:distances masked-distances})
+;; (pr/png-out masked "images/masked-maze.png")
